53992b6b3faf55caed9641491c73ef2c341aaec1
StoegerSeb <SkyCrash13241234>
2020-04-01 21:39:26 +0200

    [016] Added MapEditor - Flickering

--- OctoAwesome/MapEditor/Program.cs
+++ OctoAwesome/MapEditor/Program.cs
@@ -4,19 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace UserInterface
+namespace MapEditor
 {
     static class Program
     {
         /// <summary>
-        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new renderPanl());
         }
     }
 }
--- OctoAwesome/MapEditor/Properties/Settings.Designer.cs
+++ OctoAwesome/MapEditor/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace UserInterface.Properties
+namespace MapEditor.Properties
 {
 
 
