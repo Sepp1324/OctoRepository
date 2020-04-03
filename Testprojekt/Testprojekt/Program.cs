75aa21454d0d84a5c1091dec6c94b81a429a81d7
StoegerSeb <SkyCrash13241234>
2020-04-03 20:44:21 +0200

    Test

---
+++ Testprojekt/Testprojekt/Program.cs
@@ -0,0 +1,23 @@
+﻿using OctoAwesomeDX;
+using System;
+
+namespace Testprojekt
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new OctoGame())
+                game.Run();
+        }
+    }
+#endif
+}
