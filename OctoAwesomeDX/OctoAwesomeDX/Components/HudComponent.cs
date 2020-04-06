c23143316e27520f178fdafbf98fce9b5a9f5105
StoegerSeb <SkyCrash13241234>
2020-04-06 23:59:01 +0200

    Added reticle

--- OctoAwesomeDX/OctoAwesomeDX/Components/HudComponent.cs
+++ OctoAwesomeDX/OctoAwesomeDX/Components/HudComponent.cs
@@ -11,12 +11,22 @@ namespace OctoAwesome.Components
     internal sealed class HudComponent : DrawableGameComponent
     {
         private WorldComponent world;
+
         private SpriteBatch batch;
         private SpriteFont font;
+
+        private Texture2D pix;
+
+        private float[] frameBuffer;
+
+        private int bufferSize = 10;
+        private int bufferIndex = 0;
 
         public HudComponent(Game game, WorldComponent world): base(game)
         {
             this.world = world;
+
+            frameBuffer = new float[bufferSize];
         }
 
         public override void Initialize()
@@ -29,11 +39,15 @@ namespace OctoAwesome.Components
         protected override void LoadContent()
         {
             font = Game.Content.Load<SpriteFont>("hud");
+            pix = Game.Content.Load<Texture2D>("Textures/pix");
             base.LoadContent();
         }
 
         public override void Draw(GameTime gameTime)
         {
+            frameBuffer[bufferIndex++] = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            bufferIndex %= bufferSize;
+
             batch.Begin();
 
             batch.DrawString(font, "Development Version", new Vector2(5, 5), Color.White);
@@ -45,6 +59,18 @@ namespace OctoAwesome.Components
             string rot = "rot: " + world.World.Player.Angle.ToString("0.00") + " / " + world.World.Player.Tilt.ToString("0.00");
             size = font.MeasureString(rot); 
             batch.DrawString(font, rot, new Vector2(GraphicsDevice.Viewport.Width - size.X - 5, 25), Color.White);
+
+            string fps = "fps: " + (1f / (frameBuffer.Sum() / bufferSize)).ToString("0.00");
+            size = font.MeasureString(fps);
+            batch.DrawString(font, fps, new Vector2(GraphicsDevice.Viewport.Width - size.X - 5, 45), Color.White);
+
+            int centerX = GraphicsDevice.Viewport.Width / 2;
+            int centerY = GraphicsDevice.Viewport.Height / 2;
+
+
+
+            batch.Draw(pix, new Rectangle(centerX - 1, centerY - 15, 2, 30), Color.White * 0.5f);
+            batch.Draw(pix, new Rectangle(centerX - 15, centerY - 1, 30, 2), Color.White * 0.5f);
 
             batch.End();
         }
