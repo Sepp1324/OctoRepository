0f2a8da9adfe0af690a758365cd60f8874e1a848
StoegerSeb <SkyCrash13241234>
2020-04-22 11:22:47 +0200

    Started ResourceManager

---
+++ OctoAwesome/OctoAwesome.Runtime/ResourceManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OctoAwesome.Runtime
+{
+    public class ResourceManager
+    {
+        #region Singleton
+
+        private static ResourceManager instance = null;
+
+        public static ResourceManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ResourceManager();
+                return instance;
+            }
+        }
+
+        #endregion
+
+        private ResourceManager()
+        {
+
+        }
+
+        public IPlanet GetPlanet(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
