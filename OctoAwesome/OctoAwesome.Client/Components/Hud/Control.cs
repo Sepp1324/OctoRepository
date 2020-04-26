f20a0494063a3fa6583b52fef0ff675fe0711631
StoegerSeb <SkyCrash13241234>
2020-04-26 12:51:48 +0200

    Added Controls und Toolbar

---
+++ OctoAwesome/OctoAwesome.Client/Components/Hud/Control.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace OctoAwesome.Client.Components.Hud
+{
+    internal abstract class Control
+    {
+        public Index2 Position { get; set; }
+
+        public Index2 Size { get; set; }
+
+        protected HudComponent Hud { get; private set; }
+
+        public Control(HudComponent hud)
+        {
+            Hud = hud;
+        }
+
+        public abstract void LoadContent();
+
+        public abstract void Draw(SpriteBatch batch);
+    }
+}
