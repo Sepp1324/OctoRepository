ef0f2657e93c9f48cd15e0c78403344e3509c67a
Sebastian Stögerer <SkyCrash13241234>
2020-04-29 18:13:26 +0200

    HardReset

---
+++ OctoAwesome/OctoAwesome.Runtime/IResourceManager.cs
@@ -0,0 +1,11 @@
+﻿namespace OctoAwesome.Runtime
+{
+    public interface IResourceManager
+    {
+        IUniverse GetUniverse(int id);
+
+        IPlanet GetPlanet(int id);
+
+        IPlanetResourceManager GetManagerForPlanet(int planetId);
+    }
+}
---
+++ OctoAwesome/OctoAwesome/BlockDefinition.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Drawing;
+
+namespace OctoAwesome
+{
+    public abstract class BlockDefinition : IBlockDefinition
+    {
+        public abstract string Name { get; }
+
+        public abstract Bitmap Icon { get; }
+
+        public abstract Bitmap[] Textures { get; }
+
+        public abstract PhysicalProperties GetProperties(IPlanetResourceManager manager, int x, int y, int z);
+
+        public abstract void Hit(IBlockDefinition block, PhysicalProperties itemProperties);
+
+        public virtual BoundingBox[] GetCollisionBoxes(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return new[] { new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1)) };
+        }
+
+        public virtual int GetTopTextureIndex(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetBottomTextureIndex(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetNorthTextureIndex(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetSouthTextureIndex(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetWestTextureIndex(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetEastTextureIndex(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetTopTextureRotation(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetBottomTextureRotation(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetEastTextureRotation(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetWestTextureRotation(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetNorthTextureRotation(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual int GetSouthTextureRotation(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return 0;
+        }
+
+        public virtual bool IsTopSolidWall(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return true;
+        }
+
+        public virtual bool IsBottomSolidWall(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return true;
+        }
+
+        public virtual bool IsNorthSolidWall(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return true;
+        }
+
+        public virtual bool IsSouthSolidWall(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return true;
+        }
+
+        public virtual bool IsWestSolidWall(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return true;
+        }
+
+        public virtual bool IsEastSolidWall(IPlanetResourceManager manager, int x, int y, int z)
+        {
+            return true;
+        }
+    }
+
+    public interface IUpdateable
+    {
+        void Tick(IPlanetResourceManager manager, int x, int y, int z);
+    }
+}
