7f38a0fc78cc8da8fdd3729597675c3dabe9d9b9
Sebastian Stögerer <SkyCrash13241234>
2020-05-05 15:39:45 +0200

    Worked on Server

--- OctoAwesome/OctoAwesome.Runtime/IClient.cs
+++ OctoAwesome/OctoAwesome.Runtime/IClient.cs
@@ -1,12 +1,13 @@
-﻿using System.ServiceModel;
+﻿using System;
+using System.ServiceModel;
 
 namespace OctoAwesome.Runtime
 {
     [ServiceContract(CallbackContract = typeof(IClientCallback), SessionMode = SessionMode.Required)]
     public interface IClient
     {
-        [OperationContract(IsInitiating = true, IsTerminating = false, IsOneWay = true)]
-        void Connect(string playername);
+        [OperationContract(IsInitiating = true, IsTerminating = false)]
+        Guid Connect(string playername);
 
         [OperationContract(IsInitiating = false, IsTerminating = true, IsOneWay = true)]
         void Disconnect();
