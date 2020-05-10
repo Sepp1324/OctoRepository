cc69af097a4477994e2ae8497264f6def8f6d536
Sebastian Stögerer <SkyCrash13241234>
2020-05-11 00:24:13 +0200

    Code-CleanUp -> BlockDefinitions

--- OctoAwesome/OctoAwesome/Wall.cs
+++ OctoAwesome/OctoAwesome/Wall.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OctoAwesome
 {
-    [Flags]
     public enum Wall
     {
-        None,
         Top,
         Bottom,
         Left,
