eeafe1aa5822514096a04ff22462d01e08b73855
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-07-10 16:52:56 +0200

    Ttsdsw

--- OctoAwesome/OctoAwesome.Runtime/ResourceManager.cs
+++ OctoAwesome/OctoAwesome.Runtime/ResourceManager.cs
@@ -1,39 +1,36 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Text;
 
 namespace OctoAwesome.Runtime
 {
-    /// <summary>
-    /// Manager für die Weltelemente im Spiel.
-    /// </summary>
-    public class ResourceManager : IResourceManager
+    public class ResourceManager
     {
-        private Guid DEFAULT_UNIVERSE = Guid.Parse("{3C4B1C38-70DC-4B1D-B7BE-7ED9F4B1A66D}");
+        public static int CacheSize = 10000;
 
         private bool disablePersistence = false;
 
-        private IPersistenceManager persistenceManager = null;
-
-        private GlobalChunkCache globalChunkCache = null;
-
-        private List<IMapPopulator> populators = null;
-
-        private IUniverse universe;
-
-        private Dictionary<int, IPlanet> planets;
+        private IMapGenerator mapGenerator = null;
+        private IChunkPersistence chunkPersistence = null;
 
         /// <summary>
-        /// Das aktuell geladene Universum.
+        /// Planet Cache.
         /// </summary>
-        public IUniverse CurrentUniverse { get { return universe; } }
+        private Cache<int, IPlanet> planetCache;
+
+        /// <summary>
+        /// Chunk Cache.
+        /// </summary>
+        private Cache<PlanetIndex3, IChunk> chunkCache;
+
+        private IUniverse universeCache;
 
         #region Singleton
 
         private static ResourceManager instance = null;
-        /// <summary>
-        /// Die Instanz des ResourceManagers.
-        /// </summary>
         public static ResourceManager Instance
         {
             get
@@ -48,227 +45,142 @@ namespace OctoAwesome.Runtime
 
         private ResourceManager()
         {
-            persistenceManager = new DiskPersistenceManager();
+            mapGenerator = MapGeneratorManager.GetMapGenerators().First();
+            chunkPersistence = new ChunkDiskPersistence();
 
-            globalChunkCache = new GlobalChunkCache(
-                (p, i) => loadChunkColumn(p, i),
-                (p, i, c) => saveChunkColumn(p, i, c));
+            planetCache = new Cache<int, IPlanet>(1, loadPlanet, savePlanet);
+            chunkCache = new Cache<PlanetIndex3, IChunk>(CacheSize, loadChunk, saveChunk);
 
-            planets = new Dictionary<int, IPlanet>();
-
-            bool.TryParse(SettingsManager.Get("DisablePersistence"), out disablePersistence);
+            bool.TryParse(ConfigurationManager.AppSettings["DisablePersistence"], out disablePersistence);
         }
 
-        /// <summary>
-        /// Der <see cref="IGlobalChunkCache"/>, der im Spiel verwendet werden soll.
-        /// </summary>
-        public IGlobalChunkCache GlobalChunkCache { get { return globalChunkCache; } }
-
-        /// <summary>
-        /// Erzuegt ein neues Universum.
-        /// </summary>
-        /// <param name="name">Name des neuen Universums.</param>
-        /// <param name="seed">Weltgenerator-Seed für das neue Universum.</param>
-        /// <returns>Die Guid des neuen Universums.</returns>
-        public Guid NewUniverse(string name, int seed)
+        public IUniverse GetUniverse(int id)
         {
-            Guid guid = Guid.NewGuid();
-            universe = new Universe(guid, name, seed);
-            persistenceManager.SaveUniverse(universe);
-            return guid;
+            if (universeCache == null)
+                universeCache = mapGenerator.GenerateUniverse(id);
+
+            return universeCache;
         }
 
-        /// <summary>
-        /// Gibt alle Universen zurück, die geladen werden können.
-        /// </summary>
-        /// <returns>Die Liste der Universen.</returns>
-        public IUniverse[] ListUniverses()
-        {
-            return persistenceManager.ListUniverses();
-        }
-
-        /// <summary>
-        /// Lädt das Universum mit der angegebenen Guid.
-        /// </summary>
-        /// <param name="universeId">Die Guid des Universums.</param>
-        /// <returns>Das geladene Universum.</returns>
-        public void LoadUniverse(Guid universeId)
-        {
-            // Alte Daten entfernen
-            if (universe != null)
-                UnloadUniverse();
-
-            // Neuen Daten loaden/generieren
-            universe = persistenceManager.LoadUniverse(universeId);
-            if (universe == null)
-                throw new Exception();
-        }
-
-        /// <summary>
-        /// Entlädt das aktuelle Universum.
-        /// </summary>
-        public void UnloadUniverse()
-        {
-            // TODO: Save und Unload
-
-            // Unload Chunks
-            globalChunkCache.Clear();
-            
-            // TODO: Unload Planets
-            // TODO: Unload Universe;
-        }
-
-        /// <summary>
-        /// Entlädt das aktuelle Universum
-        /// </summary>
-        /// <returns>Das gewünschte Universum, falls es existiert</returns>
-        public IUniverse GetUniverse()
-        {
-            return universe;
-        }
-
-        /// <summary>
-        /// Löscht ein Universum.
-        /// </summary>
-        /// <param name="id">Die Guid des Universums.</param>
-        public void DeleteUniverse(Guid id)
-        {
-            if (universe != null && universe.Id == id)
-                throw new Exception("Universe ist bereits geladen");
-
-            persistenceManager.DeleteUniverse(id);
-        }
-
-        /// <summary>
-        /// Gibt den Planeten mit der angegebenen ID zurück
-        /// </summary>
-        /// <param name="id">Die Planteten-ID des gewünschten Planeten</param>
-        /// <returns>Der gewünschte Planet, falls er existiert</returns>
         public IPlanet GetPlanet(int id)
         {
-            if (universe == null)
-                throw new Exception("No Universe loaded");
-
-            IPlanet planet;
-            if (!planets.TryGetValue(id, out planet))
-            {
-                // Versuch vorhandenen Planeten zu laden
-                planet = persistenceManager.LoadPlanet(universe.Id, id);
-                if (planet == null)
-                {
-                    // Keiner da -> neu erzeugen
-                    Random rand = new Random(universe.Seed + id);
-                    var generators = MapGeneratorManager.GetMapGenerators().ToArray();
-                    int index = rand.Next(generators.Length - 1);
-                    IMapGenerator generator = generators[index];
-                    planet = generator.GeneratePlanet(universe.Id, id, universe.Seed + id);
-                    // persistenceManager.SavePlanet(universe.Id, planet);
-                }
-
-                planets.Add(id, planet);
-            }
-
-            return planet;
+            return planetCache.Get(id);
         }
 
         /// <summary>
-        /// Lädt einen Player.
+        /// Liefert den Chunk an der angegebenen Chunk-Koordinate zurück.
         /// </summary>
-        /// <param name="playername">Der Name des Players.</param>
-        /// <returns></returns>
-        public Player LoadPlayer(string playername)
+        /// <param name="index">Chunk Index</param>
+        /// <returns>Instanz des Chunks</returns>
+        public IChunk GetChunk(int planetId, Index3 index)
         {
-            if (universe == null)
-                throw new Exception("No Universe loaded");
+            IPlanet planet = GetPlanet(planetId);
 
-            Player player = persistenceManager.LoadPlayer(universe.Id, playername);
-            if (player == null)
-                player = new Player();
-            return player;
+            if (index.X < 0 || index.X >= planet.Size.X ||
+                index.Y < 0 || index.Y >= planet.Size.Y ||
+                index.Z < 0 || index.Z >= planet.Size.Z)
+                return null;
+
+            return chunkCache.Get(new PlanetIndex3(planetId, index));
         }
 
         /// <summary>
-        /// Speichert einen Player.
+        /// Liefert den Block an der angegebenen Block-Koodinate zurück.
         /// </summary>
-        /// <param name="player">Der Player.</param>
-        public void SavePlayer(Player player)
+        /// <param name="index">Block Index</param>
+        /// <returns>Block oder null, falls dort kein Block existiert</returns>
+        public IBlock GetBlock(int planetId, Index3 index)
+        {
+            IPlanet planet = GetPlanet(planetId);
+
+            index.NormalizeXY(new Index2(
+                planet.Size.X * Chunk.CHUNKSIZE_X,
+                planet.Size.Y * Chunk.CHUNKSIZE_Y));
+            Coordinate coordinate = new Coordinate(0, index, Vector3.Zero);
+
+            // Betroffener Chunk ermitteln
+            Index3 chunkIndex = coordinate.ChunkIndex;
+            if (chunkIndex.X < 0 || chunkIndex.X >= planet.Size.X ||
+                chunkIndex.Y < 0 || chunkIndex.Y >= planet.Size.Y ||
+                chunkIndex.Z < 0 || chunkIndex.Z >= planet.Size.Z)
+                return null;
+            IChunk chunk = chunkCache.Get(new PlanetIndex3(planetId, chunkIndex));
+            if (chunk == null)
+                return null;
+
+            return chunk.GetBlock(coordinate.LocalBlockIndex);
+        }
+
+        /// <summary>
+        /// Überschreibt den Block an der angegebenen Koordinate.
+        /// </summary>
+        /// <param name="index">Block-Koordinate</param>
+        /// <param name="block">Neuer Block oder null, falls der alte Bock gelöscht werden soll.</param>
+        public void SetBlock(int planetId, Index3 index, IBlock block)
+        {
+            IPlanet planet = GetPlanet(planetId);
+
+            index.NormalizeXYZ(new Index3(
+                planet.Size.X * Chunk.CHUNKSIZE_X,
+                planet.Size.Y * Chunk.CHUNKSIZE_Y,
+                planet.Size.Z * Chunk.CHUNKSIZE_Z));
+            Coordinate coordinate = new Coordinate(0, index, Vector3.Zero);
+            IChunk chunk = GetChunk(planetId, coordinate.ChunkIndex);
+            chunk.SetBlock(coordinate.LocalBlockIndex, block);
+        }
+
+        private IPlanet loadPlanet(int index)
+        {
+            IUniverse universe = GetUniverse(0);
+
+            return mapGenerator.GeneratePlanet(universe.Id, index);
+        }
+
+        private void savePlanet(int index, IPlanet value)
         {
             if (universe == null)
-                throw new Exception("No Universe loaded");
+                throw new Exception("No Universe loaded"); //TEst
 
             persistenceManager.SavePlayer(universe.Id, player);
         }
 
-        private IChunkColumn loadChunkColumn(int planetId, Index2 index)
+        private IChunk loadChunk(PlanetIndex3 index)
         {
-            IPlanet planet = GetPlanet(planetId);
+            IUniverse universe = GetUniverse(0);
+            IPlanet planet = GetPlanet(index.Planet);
 
             // Load from disk
-            IChunkColumn column11 = persistenceManager.LoadColumn(universe.Id, planet, index);
-            if (column11 == null)
+            IChunk first = chunkPersistence.Load(universe.Id, index.Planet, index.ChunkIndex);
+            if (first != null)
+                return first;
+
+            IChunk[] result = mapGenerator.GenerateChunk(planet, new Index2(index.ChunkIndex.X, index.ChunkIndex.Y));
+            if (result != null && result.Length > index.ChunkIndex.Z)
             {
-                IChunkColumn column = planet.Generator.GenerateColumn(DefinitionManager.Instance.GetBlockDefinitions(), planet, new Index2(index.X, index.Y));
-                column11 = column;
+                result[index.ChunkIndex.Z].ChangeCounter = 0;
+                return result[index.ChunkIndex.Z];
             }
 
-            IChunkColumn column00 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(-1, -1), planet.Size));
-            IChunkColumn column10 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(0, -1), planet.Size));
-            IChunkColumn column20 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(1, -1), planet.Size));
-
-            IChunkColumn column01 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(-1, 0), planet.Size));
-            IChunkColumn column21 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(1, 0), planet.Size));
-
-            IChunkColumn column02 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(-1, 1), planet.Size));
-            IChunkColumn column12 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(0, 1), planet.Size));
-            IChunkColumn column22 = GlobalChunkCache.Peek(planet.Id, Index2.NormalizeXY(index + new Index2(1, 1), planet.Size));
-
-            // Populatoren erzeugen
-            if (populators == null)
-                populators = ExtensionManager.GetInstances<IMapPopulator>().OrderBy(p => p.Order).ToList();
-
-            // Zentrum
-            if (!column11.Populated && column21 != null && column12 != null && column22 != null)
-            {
-                foreach (var populator in populators)
-                    populator.Populate(DefinitionManager.Instance, planet, column11, column21, column12, column22);
-
-                column11.Populated = true;
-            }
-
-            // Links oben
-            if (column00 != null && !column00.Populated && column10 != null && column01 != null)
-            {
-                foreach (var populator in populators)
-                    populator.Populate(DefinitionManager.Instance, planet, column00, column10, column01, column11);
-
-                column00.Populated = true;
-            }
-
-            // Oben
-            if (column10 != null && !column10.Populated && column20 != null && column21 != null)
-            {
-                foreach (var populator in populators)
-                    populator.Populate(DefinitionManager.Instance, planet, column10, column20, column11, column21);
-                column10.Populated = true;
-            }
-
-            // Links
-            if (column01 != null && !column01.Populated && column02 != null && column12 != null)
-            {
-                foreach (var populator in populators)
-                    populator.Populate(DefinitionManager.Instance, planet, column01, column11, column02, column12);
-                column01.Populated = true;
-            }
-
-            return column11;
+            return null;
         }
 
-        private void saveChunkColumn(int planetId, Index2 index, IChunkColumn value)
+        private void saveChunk(PlanetIndex3 index, IChunk value)
         {
-            if (!disablePersistence && value.Chunks.Any(c => c.ChangeCounter > 0))
+            IUniverse universe = GetUniverse(0);
+
+            if (!disablePersistence && value.ChangeCounter > 0)
             {
-                persistenceManager.SaveColumn(universe.Id, planetId, value);
+                chunkPersistence.Save(universe.Id, index.Planet, value);
+                value.ChangeCounter = 0;
             }
+        }
+
+        /// <summary>
+        /// Persistiert den Planeten.
+        /// </summary>
+        public void Save()
+        {
+            chunkCache.Flush();
         }
     }
 }
