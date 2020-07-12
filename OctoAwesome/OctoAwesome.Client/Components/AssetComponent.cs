7abc4a64d9b0664f2dea0091ace8f6502506c88d
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-07-12 03:30:46 +0200

    Worked on AssetComponent

--- OctoAwesome/OctoAwesome.Client/Components/AssetComponent.cs
+++ OctoAwesome/OctoAwesome.Client/Components/AssetComponent.cs
@@ -2,12 +2,15 @@
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace OctoAwesome.Client.Components
 {
     internal sealed class AssetComponent : DrawableGameComponent
     {
         Dictionary<string, Texture2D> cache;
+
+        string[] textureTypes = new string[] { "png", "jpg", "jpeg", "bmp" };
 
         List<string> resourcePacks = new List<string>();
 
@@ -18,16 +21,59 @@ namespace OctoAwesome.Client.Components
 
         public Texture2D LoadTexture(Type baseType, string key)
         {
-            throw new NotImplementedException();
+            if (baseType == null)
+                throw new ArgumentNullException();
 
-            string folder = baseType.Namespace;
-            //folder/key.png
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException();
 
-            if (cache.ContainsKey(folder))
-                return cache[folder];
+            string basefolder = baseType.Namespace.
+                //Replace("OctoAwesome.", "").
+                Replace('.', Path.DirectorySeparatorChar);
 
-            //TODO: Load Resources from Pack
-            //TODO: Fallback (Mod-Assembly)
+            //Cache fragen
+            if (cache.ContainsKey(basefolder))
+                return cache[basefolder];
+
+            //Versuchen Datei zu laden
+            Texture2D result = null;
+            foreach (var resourcePack in resourcePacks)
+            {
+                string localFolder = Path.Combine(resourcePack, basefolder);
+
+                foreach (var textureType in textureTypes)
+                {
+                    string filename = Path.Combine(localFolder, string.Format("{0}.{1}", key, textureType));
+                    if (File.Exists(filename))
+                    {
+                        using (var stream = File.Open(filename, FileMode.Open))
+                        {
+                            result = Texture2D.FromStream(GraphicsDevice, stream);
+                        }
+                        break;
+                    }
+                }
+
+                if (result != null)
+                    break;
+            }
+
+            //Resourcefallback
+            if(result == null)
+            {
+
+            }
+
+            if(result == null)
+            {
+                //TODO: WorstCase: CheckerTex laden
+            }
+
+            //In Cache speichern
+            if (result != null)
+                cache[basefolder] = result;
+
+            return result;
         }
     }
 }
