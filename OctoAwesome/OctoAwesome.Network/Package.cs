72bb403fc2c0d841a16102b4f058f5c9902eeba8
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-07-26 21:54:19 +0200

    Started Deserialization

--- OctoAwesome/OctoAwesome.Network/Package.cs
+++ OctoAwesome/OctoAwesome.Network/Package.cs
@@ -42,23 +42,49 @@ namespace OctoAwesome.Network
 
         }
 
-        public void SerializePackage(OctoNetworkStream stream)
+        public void DeserializePackage(OctoNetworkStream networkStream)
         {
-            if (Payload.Length + _header.Length > stream.Length)
+            ReadHead(networkStream);
+
+            if (Type == PackageType.Subhead)
             {
-                SerializeSubPackages(stream);
+                DeserializeSubPackages(networkStream);
+                return;
+            }
+        }
+
+        public void DeserializeSubPackages(OctoNetworkStream networkStream)
+        {
+            var firstPackage = (int)networkStream.Length - SUB_HEAD_LENGTH;
+            var contentPackage = (int)networkStream.Length - SUB_CONTENT_HEAD_LENGTH;
+
+            networkStream.Read(Payload, 0, 2);
+
+            var count = Payload[0] << 8 | Payload[1];
+
+            networkStream.Read(Payload, 0, firstPackage);
+
+            for (int i = 0; i < count; i++)
+                networkStream.Read(Payload, firstPackage + contentPackage * i, contentPackage);
+        }
+
+        public void SerializePackage(OctoNetworkStream networkStream)
+        {
+            if (Payload.Length + _header.Length > networkStream.Length)
+            {
+                SerializeSubPackages(networkStream);
                 return;
             }
             Type = PackageType.Normal;
             WriteHead(ref _header);
-            stream.Write(_header, 0, _header.Length);
-            stream.Write(Payload, 0, Payload.Length);
+            networkStream.Write(_header, 0, _header.Length);
+            networkStream.Write(Payload, 0, Payload.Length);
         }
 
-        public void SerializeSubPackages(OctoNetworkStream stream)
+        public void SerializeSubPackages(OctoNetworkStream networkStream)
         {
-            var firstPackage = (int)stream.Length - SUB_HEAD_LENGTH;
-            var contentPackage = (int)stream.Length - SUB_CONTENT_HEAD_LENGTH;
+            var firstPackage = (int)networkStream.Length - SUB_HEAD_LENGTH;
+            var contentPackage = (int)networkStream.Length - SUB_CONTENT_HEAD_LENGTH;
             var count = (int)Math.Round((double)((Payload.Length - firstPackage) / contentPackage), MidpointRounding.AwayFromZero);
             var offset = firstPackage;
 
@@ -66,21 +92,21 @@ namespace OctoAwesome.Network
             WriteHead(ref _header);
             _header[8] = (byte)(count >> 8);
             _header[9] = (byte)(count & 0xFF);
-            stream.Write(_header, 0, _header.Length);
-            stream.Write(Payload, 0, firstPackage);
+            networkStream.Write(_header, 0, _header.Length);
+            networkStream.Write(Payload, 0, firstPackage);
             Type = PackageType.Subcontent;
 
             for (int i = 0; i < count - 1; i++)
             {
                 WriteHead(ref _header);
-                stream.Write(_header, 0, _header.Length);
-                stream.Write(Payload, offset, contentPackage);
+                networkStream.Write(_header, 0, _header.Length);
+                networkStream.Write(Payload, offset, contentPackage);
                 offset += contentPackage;
             }
 
             WriteHead(ref _header);
-            stream.Write(_header, 0, _header.Length);
-            stream.Write(Payload, offset, Payload.Length - offset);
+            networkStream.Write(_header, 0, _header.Length);
+            networkStream.Write(Payload, offset, Payload.Length - offset);
         }
 
         public void WriteHead(ref byte[] buffer, int offset = 0)
@@ -112,6 +138,35 @@ namespace OctoAwesome.Network
                     bytes = BitConverter.GetBytes(Uid);
                     Array.Copy(bytes, 0, header, index++, bytes.Length);
                     break;
+                case PackageType.None:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void ReadHead(OctoNetworkStream networkStream)
+        {
+            var buffer = new byte[1];
+            networkStream.Read(buffer, 0, 1);
+            Type = (PackageType)buffer[0];
+
+            switch (Type)
+            {
+                case PackageType.Normal:
+                    buffer = new byte[HEAD_LENGTH - 1];
+
+                    networkStream.Read(buffer, 0, buffer.Length);
+                    Command = (ushort)(buffer[0] << 8 | buffer[1]);
+                    break;
+                case PackageType.Subhead:
+                    buffer = new byte[SUB_HEAD_LENGTH - 1];
+
+                    networkStream.Read(buffer, 0, buffer.Length);
+                    Command = (ushort)(buffer[0] << 8 | buffer[1]);
+                    Uid = BitConverter.ToUInt64(buffer, 2);
+                    break;
+                case PackageType.Subcontent:
                 case PackageType.None:
                     break;
                 default:
