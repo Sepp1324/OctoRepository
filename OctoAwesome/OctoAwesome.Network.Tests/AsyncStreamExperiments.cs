8f571391ac4ca3f8c81e56747ae9d8630dcccfef
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-07-27 23:51:09 +0200

    Worked on Networkbuffer

--- OctoAwesome/OctoAwesome.Network.Tests/AsyncStreamExperiments.cs
+++ OctoAwesome/OctoAwesome.Network.Tests/AsyncStreamExperiments.cs
@@ -8,28 +8,44 @@ namespace OctoAwesome.Network.Tests
     {
         private class TestAsyncStream
         {
-            public readonly byte[] ReadBuffer;
-            public readonly byte[] WriteBuffer;
+            private byte[] _readBuffer;
+            private byte[] _writeBuffer;
+
+            private readonly byte[] _bufferA;
+            private readonly byte[] _bufferB;
+
+            private readonly int writeLength;
+            private readonly int readLength;
 
             private int _readPosition;
             private int _writePosition;
 
             public TestAsyncStream(int capacity = 1024)
             {
-                ReadBuffer = new byte[2024];
-                WriteBuffer = new byte[1024];
+                _bufferB = new byte[capacity];
+                _bufferA = new byte[capacity];
+                _readBuffer = _bufferA;
+                _writeBuffer = _bufferB;
+                readLength = capacity;
+                writeLength = capacity;
                 _writePosition = 0;
                 _readPosition = 0;
             }
 
-            public void Write()
+            public int Write(byte[] buffer, int offset, int count)
             {
-
+                var realCopy = _writePosition - writeLength;
+                Buffer.BlockCopy(buffer, offset, _writeBuffer, _writePosition, realCopy); 
+                _writePosition += realCopy;
+                return realCopy;
             }
 
-            public void Read()
+            public int Read(byte[] buffer, int offset, int count)
             {
-
+                var realCopy = _readPosition - readLength;
+                Array.Copy(_readBuffer, _readPosition, buffer, offset, realCopy);
+                _readPosition += realCopy;
+                return realCopy;
             }
         }
     }
