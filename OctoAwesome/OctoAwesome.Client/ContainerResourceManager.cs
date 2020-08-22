adadb31bac7f061f3d6ad7d1b007107b8c7e5370
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-08-22 17:33:20 +0200

    Synced

--- OctoAwesome/OctoAwesome.Client/ContainerResourceManager.cs
+++ OctoAwesome/OctoAwesome.Client/ContainerResourceManager.cs
@@ -2,10 +2,6 @@
 using OctoAwesome.Notifications;
 using OctoAwesome.Runtime;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OctoAwesome.Client
 {
