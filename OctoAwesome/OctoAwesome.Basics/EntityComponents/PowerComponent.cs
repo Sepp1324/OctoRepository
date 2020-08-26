7fba0c2d2f80480cd911fd445c1294e749da2dd7
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-08-27 00:05:17 +0200

    Worked on PowerComponent

--- OctoAwesome/OctoAwesome.Basics/EntityComponents/PowerComponent.cs
+++ OctoAwesome/OctoAwesome.Basics/EntityComponents/PowerComponent.cs
@@ -1,9 +1,4 @@
 ﻿using engenious;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace OctoAwesome.Basics.EntityComponents
@@ -14,17 +9,15 @@ namespace OctoAwesome.Basics.EntityComponents
 
         public Vector3 Direction { get; set; }
 
-        public override void Serialize(BinaryWriter writer, IDefinitionManager definitionManager)
+        public override void Serialize(BinaryWriter writer)
         {
-            base.Serialize(writer, definitionManager);
-
+            base.Serialize(writer);
             writer.Write(Power);
         }
 
-        public override void Deserialize(BinaryReader reader, IDefinitionManager definitionManager)
+        public override void Deserialize(BinaryReader reader)
         {
-            base.Deserialize(reader, definitionManager);
-
+            base.Deserialize(reader);
             Power = reader.ReadSingle();
         }
     }
