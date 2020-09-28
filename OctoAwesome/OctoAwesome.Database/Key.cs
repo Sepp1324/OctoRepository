8cd1f607bfa40c437d986363d1d655a00b715ef0
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-09-28 16:36:43 +0200

    Implemented Key

---
+++ OctoAwesome/OctoAwesome.Database/Key.cs
@@ -0,0 +1,13 @@
+﻿namespace OctoAwesome.Database
+{
+    class Key
+    {
+        public const int KEY_SIZE = sizeof(long) + sizeof(int) + sizeof(int);
+
+        public int Target { get; set; }
+
+        public long Index { get; set; }
+
+        public int Length { get; set; }
+    }
+}
