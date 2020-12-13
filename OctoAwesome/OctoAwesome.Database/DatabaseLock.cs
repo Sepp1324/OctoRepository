4f3ee843638ad61820e32513ed5b566d4ba07159
Sebastian Stögerer <stoegerer.sebastian@aon.at>
2020-12-13 22:02:09 +0100

    Worked on Database

--- OctoAwesome/OctoAwesome.Database/DatabaseLock.cs
+++ OctoAwesome/OctoAwesome.Database/DatabaseLock.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace OctoAwesome.Database
+namespace OctoAwesome.Database.Threading
 {
     public readonly struct DatabaseLock : IDisposable
     {
--- OctoAwesome/OctoAwesome.Database/Threading/DatabaseLockMonitor.cs
+++ OctoAwesome/OctoAwesome.Database/Threading/DatabaseLockMonitor.cs
@@ -1,59 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 
 namespace OctoAwesome.Database.Threading
 {
     internal sealed class DatabaseLockMonitor
     {
-        private int readLock;
-        private int writeLock;
-        private int exclusiveLock;
+        private int _readLock;
+        private int _writeLock;
+        private int _exclusiveLock;
+
+        private readonly ManualResetEvent _readEvent;
+        private readonly ManualResetEvent _writeEvent;
 
         public DatabaseLockMonitor()
         {
-            readLock = 0;
-            writeLock = 0;
-            exclusiveLock = 0;
+            _readEvent = new ManualResetEvent(true);
+            _writeEvent = new ManualResetEvent(true);
+
+            _readLock = 0;
+            _writeLock = 0;
+            _exclusiveLock = 0;
         }
 
         public bool CheckLock(Operation operation)
         {
-            if (exclusiveLock > 0)
+            if (_exclusiveLock > 0)
                 return false;
 
             if (operation.HasFlag(Operation.Read))
-                return writeLock < 1;
+                return _writeLock < 1;
 
             if (operation.HasFlag(Operation.Write))
-                return readLock < 1;
+                return _readLock < 1;
 
             return true;
+        }
+
+        public void Wait(Operation operation)
+        {
+            if (operation.HasFlag(Operation.Read))
+                _writeEvent.WaitOne();
+
+            if (operation.HasFlag(Operation.Write))
+                _readEvent.WaitOne();
         }
 
         public void SetLock(Operation operation)
         {
             if (operation.HasFlag(Operation.Exclusive))
-                Interlocked.Increment(ref exclusiveLock);
+                Interlocked.Increment(ref _exclusiveLock);
 
             if (operation.HasFlag(Operation.Read))
-                Interlocked.Increment(ref readLock);
+                Interlocked.Increment(ref _readLock);
 
             if (operation.HasFlag(Operation.Write))
-                Interlocked.Increment(ref writeLock);
+                Interlocked.Increment(ref _writeLock);
         }
 
         public void ReleaseLock(Operation operation)
         {
             if (operation.HasFlag(Operation.Exclusive))
-                Interlocked.Decrement(ref exclusiveLock);
+                Interlocked.Decrement(ref _exclusiveLock);
 
             if (operation.HasFlag(Operation.Read))
-                Interlocked.Decrement(ref readLock);
+                Interlocked.Decrement(ref _readLock);
 
             if (operation.HasFlag(Operation.Write))
-                Interlocked.Decrement(ref writeLock);
+                Interlocked.Decrement(ref _writeLock);
         }
     }
 }
